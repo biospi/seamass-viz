79a0456af1be07a72c980a93d2daf94806478e4f
Andrew Dowsey <andrew@dowsey.net>
2015-05-29 00:37:14 +0100

    Updated README.md

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Streaming visualisation of SMV files created by seaMass
 
 building
 -------
-depends on _Boost_, _Eigen_ and _libspatialindex_. **Note: _libspatialindex_ MUST be at least version 1.8.5**. Windows and Mac developers can get these from the [dependency repository](https://github.com/biospi/seamass-windeps).
+depends on _CMake_, _Boost_, _Eigen_ and _libspatialindex_. **Note: _libspatialindex_ MUST be at least version 1.8.5**.
+Windows and Mac developers can get these from the [dependency repository](https://github.com/biospi/seamass-windeps).
 
 running
 -------
--- cmake_intel14_msvc2013_32.bat
+++ cmake_intel14_msvc2013_32.bat
@@ -8,14 +8,14 @@ mkdir build\%SEAMASS_TOOLSET%
 set "SEAMASS_BUILD=debug"
 mkdir build\%SEAMASS_TOOLSET%\%SEAMASS_BUILD%
 pushd build\%SEAMASS_TOOLSET%\%SEAMASS_BUILD%
-cmake -G"Visual Studio 11 2012" -T"Intel C++ Compiler XE 15.0" -C"%~dp0..\seamass-windeps\build.cmake" %* "%~dp0"
+cmake -G"Visual Studio 12 2013" -T"Intel C++ Compiler XE 14.0" -C"%~dp0..\seamass-windeps\build.cmake" %* "%~dp0"
 if %errorlevel% neq 0 goto eof
 popd
 
 set "SEAMASS_BUILD=release"
 mkdir build\%SEAMASS_TOOLSET%\%SEAMASS_BUILD%
 pushd build\%SEAMASS_TOOLSET%\%SEAMASS_BUILD%
-cmake -G"Visual Studio 11 2012" -T"Intel C++ Compiler XE 15.0" -C"%~dp0..\seamass-windeps\build.cmake" %* "%~dp0"
+cmake -G"Visual Studio 12 2013" -T"Intel C++ Compiler XE 14.0" -C"%~dp0..\seamass-windeps\build.cmake" %* "%~dp0"
 if %errorlevel% neq 0 goto eof
 
 :eof
